a7580f411c2f2a116109aec9b9cc2d34628f9424
usausa <machi.pon@gmail.com>
2024-02-06 13:07:00 +0900

    Update setting

--- Template.Functions/Functions/Log.cs
+++ Template.Functions/Functions/Log.cs
@@ -3,7 +3,7 @@ namespace Template.Functions.Functions;
 using Microsoft.Azure.WebJobs.Extensions.Timers;
 
 #pragma warning disable SYSLIB1006
-public static partial class Log
+internal static partial class Log
 {
     [LoggerMessage(Level = LogLevel.Information, Message = "C# HTTP trigger function processed a request.")]
     public static partial void InfoHttpTrigger(this ILogger logger);
