a2a921ad6890d5359283b6ee268f6a0b3b21d76c
usausa <machi.pon@gmail.com>
2024-02-06 13:49:52 +0900

    Update setting

--- Template.Functions/Infrastructure/Log.cs
+++ Template.Functions/Infrastructure/Log.cs
@@ -1,6 +1,6 @@
 namespace Template.Functions.Infrastructure;
 
-public static partial class Log
+internal static partial class Log
 {
     [LoggerMessage(Level = LogLevel.Error, Message = "Unhandled exception. function=[{function}]")]
     public static partial void ErrorUnknownException(this ILogger logger, Exception ex, string function);
