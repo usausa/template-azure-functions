70aa9e20a916af3f002b60b8fa4456a053383888
usausa <machi.pon@gmail.com>
2024-02-08 18:57:14 +0900

    Update baseline

--- Template.Functions/Functions/Log.cs
+++ Template.Functions/Functions/Log.cs
@@ -2,7 +2,6 @@ namespace Template.Functions.Functions;
 
 using Microsoft.Azure.WebJobs.Extensions.Timers;
 
-#pragma warning disable SYSLIB1006
 internal static partial class Log
 {
     [LoggerMessage(Level = LogLevel.Information, Message = "C# HTTP trigger function processed a request.")]
@@ -11,4 +10,3 @@ internal static partial class Log
     [LoggerMessage(Level = LogLevel.Information, Message = "C# Timer trigger function executed at: {dateTime} {status}")]
     public static partial void InfoTimerTrigger(this ILogger logger, DateTime dateTime, ScheduleStatus status);
 }
-#pragma warning restore SYSLIB1006
